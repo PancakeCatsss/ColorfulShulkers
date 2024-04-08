23403751e8931138503e85fea9f03ce16587c6f3
PancakeCatsss <Aaron_jennings2003@hotmail.co.uk>
2024-04-08 18:35:17 +0100

    Initial Commit

---
+++ ColorfulShulkers/src/me/bobcatsss/colorfulshulkers/ColorfulShulkers.java
@@ -0,0 +1,16 @@
+package me.bobcatsss.colorfulshulkers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ColorfulShulkers extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
